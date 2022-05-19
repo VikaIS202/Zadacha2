95a3f31aeaf74fc278747d8b3d717f19b9e81652
VikaIS202 <105592025+VikaIS202@users.noreply.github.com>
2022-05-19 22:40:57 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Задача_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 275);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(646, 334);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите количество жюри (min 3, max 10)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(322, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите количество фигуристов";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(488, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(488, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 72);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Выставить оценки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(735, 275);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(324, 329);
+            this.listBox1.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.textBox3.Location = new System.Drawing.Point(67, 239);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(646, 30);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "Ж ю р и";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.textBox4.Location = new System.Drawing.Point(35, 275);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(26, 334);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "Ф и г у р и с т ы";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.textBox5.Location = new System.Drawing.Point(735, 239);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(324, 30);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "Р е з у л ь т а т ы";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 612);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(528, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "*В последнем столбце указана нумерация фигуристов";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 72);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Очистить результаты";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 641);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Задача 2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задача_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text); //жюри
+            int m = Convert.ToInt32(textBox2.Text); //фигуристы
+            double[,] a = new double[m, n]; //создаем вещественный массив для выставления оценок
+            Random otsenka = new Random(); //создаем рандом, чтобы оценки выставлялись автоматически
+            dataGridView1.RowCount = m; //присваиваем строкам фигуристов
+            dataGridView1.ColumnCount = n+1; //присваиваем столбцам жюри (+1 для добавления еще одного столбца для нумерации фигуристов)
+            double sum = 0; //создаем переменную для суммы
+            double rez; //в этой переменной будет результат
+            int numer = n; //для нумерации фигуристов
+            if (n > 2 && n < 11) //делаем условие, чтобы жюри соответствовало условию задачи
+            {
+                for (int i = 0; i < m; i++) //запускаем цикл для заполнения строк
+                {
+                    for (int j = 0; j < n; j++) //запускаем цикл для заполнения столбцов
+                    {
+                        a[i, j] = Convert.ToDouble(otsenka.Next(0, 100) / 10.0); //заполняем массив вещественными числами
+                        dataGridView1.Rows[i].Cells[j].Value = a[i, j]; //выводим результат в dataGridView1
+                    }
+                }
+                for (int i = 0; i < dataGridView1.RowCount; i++) //запускаем цикл для нумерации фигуристов
+                {
+                    dataGridView1.Rows[i].Cells[numer].Value = i + 1; //заполняем массив (столбец) этой нумерацией
+                }
+                for (int i = 0; i < m; i++) //запускаем цикл для поиска max и min результатов
+                {
+                    double max = 0; //переменная для записи максимального (каждый раз возвращает 0 значение)
+                    double min = a[i, 0]; //переменная для записи минимального
+                    for (int j = 0; j < n; j++) //цикл для поиска max и min результатов
+                    {
+                        sum += a[i, j]; //складываем все элементы строки
+                        if (a[i, j] > max) //условие для поиска max
+                        {
+                            max = a[i, j]; //max принимает значение элемента, если условие оказалось истинным
+                        }
+                        if (a[i, j] < min)//условие для поиска min
+                        {
+                            min = a[i, j]; //min принимает значение элемента, если условие оказалось истинным
+                        }
+                    }
+                    rez = (sum - min - max) / (n - 2); /*присваивание результатной переменной,
+                    соответствующее критериям условия (вычитаются значения max и min из общей суммы 
+                    и делятся на все количество судей, но минус 2, т.к. их оценки были убраны */
+                    listBox1.Items.Add(String.Format("Результат {0}-го фигуриста: ", i + 1) + Math.Round(rez, 1)); //вывод результата в listBox1 с одним знаком после запятой
+                    sum = 0; //сброс суммы для дальнейших операций
+                }
+            }
+            else { if (n ==0) MessageBox.Show("Без жюри мы не можем начать чемпионат!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); /* Если условие является ложным, то в зависимости от 
+            количества жюри будет всплывать окно с ошибкой*/
+                else if (n < 3) MessageBox.Show("Слишком маленькое количество жюри!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else MessageBox.Show("Слишком большое количество жюри!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); //очистка старых результатов, чтобы не путать с новыми вычислениями
+        }
+    }
+}
